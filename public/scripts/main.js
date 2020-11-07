ee494e0bd346b55fa4ccd8c7c229543861a46f86
schluncg <49917609+cschlundt@users.noreply.github.com>
2020-11-07 18:30:43 -0500

    implemented or modified the HTML, CSS, and JS needed for the login page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,36 +1,85 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
-
 /** namespace. */
 var rhit = rhit || {};
 
 /** globals */
 rhit.variableName = "";
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
+rhit.authMan = null;
 
-rhit.ClassName = class {
+/** Page controllers */
+rhit.LoginPageController = class {
 	constructor() {
+		document.getElementById("rosefireButton").onclick = (event) => {
+			rhit.authMan.signIn();
+		};
+	}
+}
 
+
+/** Managers */
+rhit.AuthenticationManager = class {
+	constructor() {
+		this._user = null;
 	}
 
-	methodName() {
-
+	beginListening(changeListener) {
+		firebase.auth().onAuthStateChanged((user) => {
+			this._user = user;
+			changeListener();
+		});
 	}
+
+	signIn(){
+		//TODO: implement loging in via RoseFire
+	}
+
+	signOut() {
+		firebase.auth().signOut().catch((error) => {
+			console.log("Sign out error");
+		});
+	}
+
+	get isSignedIn() {return !!this._user;}
+	get uid() {return this._user.uid};
+}
+
+rhit.initalizePage = () => {
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+
+	if(document.querySelector("#loginPage")) {
+		rhit.startFirebaseUI();
+		new rhit.LoginPageController();
+	}
+}
+
+rhit.checkForRedirects = () => {
+
+}
+
+rhit.startFirebaseUI= () => {
+	var uiConfig = {
+        signInSuccessUrl: '/homepage.html',
+        signInOptions: [
+			firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+			firebase.auth.EmailAuthProvider.PROVIDER_ID,
+			firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+			firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+        ],
+      };
+      const ui = new firebaseui.auth.AuthUI(firebase.auth());
+      ui.start('#firebaseui-auth-container', uiConfig);
 }
 
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+	rhit.authMan = new rhit.AuthenticationManager();
+	rhit.authMan.beginListening(() => {
+		rhit.checkForRedirects();
+		rhit.initalizePage();
+	})
 };
 
 rhit.main();
